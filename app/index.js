60896405e3cb9da0e38a52fe3592287c6ea39cbb
i9b4r1a9h5i7m6 <mohamedibrahim35@gmail.com>
2017-06-02 04:34:41 -0700

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,17 +5,12 @@ var express =   require("express");
   
 var app =   express();  
 
-
-
-
-
-    
-    app.get('/',function(req,res){  
+app.get('/',function(req,res){  
         res.sendFile(__dirname + "/index.html");  
     });  
     
     
-    });  
+  
   
 app.listen(5000,function(){  
     console.log("Server is running on port 2000");  
